a80dc3fdef34661324e625630f8cbba21842cca6
Sergey <jedipower999@gmail.com>
2019-09-18 16:28:31 +0300

    THE BEST

---
+++ project/LESSON 1/script.js
@@ -0,0 +1,61 @@
+'use strict';
+'use maxlen';
+
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// console.log(second);
+
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*9);
+
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 21,
+    isMarried: false  
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[0]);
+
+// alert("Hello World!");
+
+// let answer = prompt("Как Вас зовут?", "НЕО");
+
+// console.log(answer);
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(decr--);
+
+console.log(5%2);
+console.log("8" == 8);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked && !isClose);
+console.log(isChecked || isClose);
+
+
+
+
+
